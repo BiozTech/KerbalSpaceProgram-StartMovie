673b87a6ec141c4a269c84eae88058a33621d8dd
BiozTech <61115267+BiozTech@users.noreply.github.com>
2021-03-05 19:34:23 +0300

    Alpha 0.10

---
+++ GUI.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace StartMovie
+{
+
+	[KSPAddon(KSPAddon.Startup.EveryScene, false)]
+
+	public class StartMovieGUI : MonoBehaviour
+	{
+
+		static string strKeyRecord = Settings.KeyRecord.ToString();
+		static string strFramerate = Settings.Framerate.ToString();
+		static string strSuperSize = Settings.SuperSize.ToString();
+		static string strDirectory = Settings.ShotsDirectoryToOutput;
+
+		static bool isGUIVisible = false;
+		static bool isStrKeyRecordOk = true;
+		static bool isStrFramerateOk = true;
+		static bool isStrSuperSizeOk = true;
+		static bool isStrDirectoryOk = true;
+
+		static Rect windowPosition = new Rect(100, 250, 0, 0);
+		static GUILayoutOption TFSettingWidth = GUILayout.Width(150f);
+
+		void OnGUI()
+		{
+			if (isGUIVisible)
+			{
+				windowPosition = GUILayout.Window(0, windowPosition, OnWindow, "StartMovie Settings");
+			}
+		}
+
+		void OnWindow(int windowId)
+		{
+
+			GUIStyle TFstyle = new GUIStyle(GUI.skin.textField);
+			GUILayout.BeginVertical(GUILayout.Width(250f));
+
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Key");
+			ColorizeTextField(TFstyle, isStrKeyRecordOk);
+			strKeyRecord = GUILayout.TextField(strKeyRecord, TFstyle, TFSettingWidth);
+			GUILayout.EndHorizontal();
+
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Framerate");
+			ColorizeTextField(TFstyle, isStrFramerateOk);
+			strFramerate = GUILayout.TextField(strFramerate, TFstyle, TFSettingWidth);
+			GUILayout.EndHorizontal();
+
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("SuperSize");
+			ColorizeTextField(TFstyle, isStrSuperSizeOk);
+			strSuperSize = GUILayout.TextField(strSuperSize, TFstyle, TFSettingWidth);
+			GUILayout.EndHorizontal();
+
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Screenshots directory");
+			ColorizeTextField(TFstyle, isStrDirectoryOk);
+			strDirectory = GUILayout.TextField(strDirectory, TFstyle, TFSettingWidth);
+			GUILayout.EndHorizontal();
+
+			GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			if (GUILayout.Button("Apply", GUILayout.Width(100f)))
+			{
+
+				bool isOk = true;
+
+				KeyCode key;
+				isStrKeyRecordOk = Enum.TryParse(strKeyRecord, out key);
+				if (isStrKeyRecordOk) Settings.KeyRecord = key;
+				isOk &= isStrKeyRecordOk;
+
+				int fps;
+				isStrFramerateOk = Int32.TryParse(strFramerate, out fps);
+				if (isStrFramerateOk) Settings.Framerate = fps;
+				isOk &= isStrFramerateOk;
+
+				int size;
+				isStrSuperSizeOk = Int32.TryParse(strSuperSize, out size);
+				if (isStrSuperSizeOk) Settings.SuperSize = size;
+				isOk &= isStrSuperSizeOk;
+
+				if (strDirectory != string.Empty)
+				{
+					isStrDirectoryOk = Directory.Exists(strDirectory);
+					if (isStrDirectoryOk) Settings.ShotsDirectory = strDirectory;
+					isOk &= isStrDirectoryOk;
+				} else {
+					isStrDirectoryOk = true;
+					Settings.ShotsDirectory = Settings.ShotsDirectoryDefault;
+				}
+
+				if (isOk) Settings.Save();
+
+			}
+			GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+
+			GUILayout.EndVertical();
+
+			GUI.DragWindow();
+
+		}
+
+		static void ColorizeTextField(GUIStyle style, bool isStrOk)
+		{
+			style.normal.textColor = style.focused.textColor = style.hover.textColor = (isStrOk) ? Color.white : Color.red;
+		}
+
+		public static void Toggle()
+		{
+			isGUIVisible = !isGUIVisible;
+		}
+
+		public static void Initialize()
+		{
+			strKeyRecord = Settings.KeyRecord.ToString();
+			strFramerate = Settings.Framerate.ToString();
+			strSuperSize = Settings.SuperSize.ToString();
+			strDirectory = Settings.ShotsDirectoryToOutput;
+		}
+
+	}
+
+}
---
+++ Settings.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace StartMovie
+{
+
+	public static class Settings
+	{
+
+		public static KeyCode KeyRecord = KeyCode.F6;
+		public static int Framerate = 60;
+		public static int SuperSize = GameSettings.SCREENSHOT_SUPERSIZE;
+		public static string ShotsDirectoryDefault = Path.Combine(KSPUtil.ApplicationRootPath, "Screenshots");
+		public static string ShotsDirectory = ShotsDirectoryDefault;
+
+		static string pluginDataDir = Path.Combine(KSPUtil.ApplicationRootPath, "GameData/StartMovie/PluginData");
+		static string settingsFileName = Path.Combine(pluginDataDir, "settings.cfg");
+		static string configTagMain = "StartMovieSettings";
+		static string configTagKey = "Key";
+		static string configTagFramerate = "Framerate";
+		static string configTagSize = "SuperSize";
+		static string configTagShotsDirectory = "ScreenshotsDirectory";
+
+		// optional
+	//	public static float DeltaTimeLimit = GameSettings.PHYSICS_FRAME_DT_LIMIT; // 0.02 ~ 0.35 -> inf
+	//	static string configTagDeltaTimeLimit = "DeltaTimeLimit";
+
+		public static void Load()
+		{
+			if (!File.Exists(settingsFileName))
+			{
+				Save();
+				Log("Have no settings file, huh? Here is one for you (づ｡◕‿‿◕｡)づ");
+				return;
+			}
+			ConfigNode configNode = ConfigNode.Load(settingsFileName);
+			if (!configNode.HasNode(configTagMain))
+			{
+				BackupAndSave();
+				Log("General Failure reading settings file");
+				return;
+			}
+			configNode = configNode.GetNode(configTagMain); // ;)
+			try
+			{
+
+				bool isOk = true;
+				bool isOkCurrent;
+
+				isOkCurrent = configNode.HasValue(configTagKey);
+				if (isOkCurrent)
+				{
+					KeyCode key;
+					isOkCurrent = Enum.TryParse(configNode.GetValue(configTagKey), out key);
+					if (isOkCurrent) KeyRecord = key;
+				}
+				isOk &= isOkCurrent;
+
+				isOkCurrent = configNode.HasValue(configTagFramerate);
+				if (isOkCurrent)
+				{
+					int fps;
+					isOkCurrent = Int32.TryParse(configNode.GetValue(configTagFramerate), out fps);
+					if (isOkCurrent) Framerate = fps;
+				}
+				isOk &= isOkCurrent;
+
+				isOkCurrent = configNode.HasValue(configTagSize);
+				if (isOkCurrent)
+				{
+					int size;
+					isOkCurrent = Int32.TryParse(configNode.GetValue(configTagSize), out size);
+					if (isOkCurrent) SuperSize = size;
+				}
+				isOk &= isOkCurrent;
+
+				isOkCurrent = configNode.HasValue(configTagShotsDirectory);
+				if (isOkCurrent)
+				{
+					string directory = configNode.GetValue(configTagShotsDirectory);
+					if (directory != string.Empty)
+					{
+						isOkCurrent = Directory.Exists(directory);
+						if (isOkCurrent) ShotsDirectory = directory;
+					}
+				}
+				isOk &= isOkCurrent;
+
+				// optional
+			//	if (configNode.HasValue(configTagDeltaTimeLimit))
+			//	{
+			//		float dtLimit;
+			//		isOkCurrent = float.TryParse(configNode.GetValue(configTagDeltaTimeLimit).Replace(',', '.'), System.Globalization.NumberStyles.Number, System.Globalization.CultureInfo.InvariantCulture.NumberFormat, out dtLimit);
+			//		if (isOkCurrent) DeltaTimeLimit = dtLimit;
+			//	}
+
+				if (!isOk)
+				{
+					BackupAndSave();
+					Log("General Failure reading settings file");
+				}
+
+			}
+			catch (Exception stupid)
+			{
+				BackupAndSave();
+				Log("Not to worry, we are still flying half a ship.");
+				Log(stupid.Message);
+			}
+			StartMovieGUI.Initialize();
+		}
+
+		public static void Save()
+		{
+			if (!Directory.Exists(pluginDataDir)) Directory.CreateDirectory(pluginDataDir);
+			ConfigNode configNode = new ConfigNode(configTagMain);
+			configNode.AddValue(configTagKey, KeyRecord, "Key to start recording (Alt+Key to open menu)");
+			configNode.AddValue(configTagFramerate, Framerate, "Target framerate");
+			configNode.AddValue(configTagSize, SuperSize, "SCREENSHOT_SUPERSIZE");
+			configNode.AddValue(configTagShotsDirectory, ShotsDirectoryToOutput, "Folder the shots will be saved to");
+		//	configNode.AddValue(configTagDeltaTimeLimit, DeltaTimeLimit, "Affects on physics accuracy");
+			File.WriteAllText(settingsFileName, configNode.ToString(), System.Text.Encoding.Unicode); // non-latin letters? nah, never heard
+		}
+		static void BackupAndSave()
+		{
+			string settingsFileBackupName = settingsFileName + ".bak";
+			if (File.Exists(settingsFileBackupName)) File.Delete(settingsFileBackupName);
+			File.Move(settingsFileName, settingsFileBackupName);
+			Save();
+		}
+
+		public static string ShotsDirectoryToOutput
+		{
+			get
+			{
+				return (ComparePaths(ShotsDirectory, ShotsDirectoryDefault)) ? string.Empty : ShotsDirectory;
+			}
+		}
+		public static bool ComparePaths(string path1, string path2)
+		{
+			return (String.Compare(Path.GetFullPath(path1).TrimEnd('\\'), Path.GetFullPath(path2).TrimEnd('\\'), StringComparison.InvariantCultureIgnoreCase) == 0);
+		}
+
+		public static void Log(object message)
+		{
+			Debug.Log("<color=#ffcc33>StartMovie</color> » " + message);
+		}
+
+	}
+
+}
